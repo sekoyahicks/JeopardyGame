9acf62e90f7416d73efd4a9571ef5ead44933e81
Sekoya Hicks <sekoyahicks@Sekoyas-MacBook-Pro.local>
2019-04-12 13:43:08 -0400

    Added additional question to see if for loop truly works.

--- project1.js
+++ project1.js
@@ -14,6 +14,7 @@ let uniqueId;
 
 $(function() {
   $(".square").on("click", function() {
+    console.log('hello')
     uniqueId = $(this).attr("id")
    console.log(uniqueId);
     $(".modal-background").show();
@@ -24,23 +25,190 @@ $(function() {
         console.log("hi")
         /*push qArray[i] to modal*/
         $(".question").html(qArray[i].question);
-      }
-    };
-    
+        $(".answer1").html(qArray[i].answer1);
+        $(".answer2").html(qArray[i].answer2);
+        $(".answer3").html(qArray[i].answer3);
+      };
+    };  
   });
 });
 
-qArray= [
+qArray = [
   {
   id:"0100",
   question: "What liquid is the most hydrating?",
   answer1: "soda",
   answer2: "water",
   answer3: "milk"
-}
-];
-
-
- 
+},
+{
+  id:"1100",
+  question: "sidjofw",
+  answer1: "sdjfoew",
+  answer2: "dsafw",
+  answer3: "sdfaw"
+},
+/*{
+  id:"2100",
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:"3100",
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:"4100"
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:""
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},
+{
+  id:
+  question:
+  answer1:
+  answer2:
+  answer3:
+},*/
+]
 
 
