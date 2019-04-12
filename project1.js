c065e14ab6012a42c16257889858b1b49c69452e
Sekoya Hicks <sekoyahicks@Sekoyas-MacBook-Pro.local>
2019-04-12 11:54:04 -0400

    Question shows in modal now.

--- project1.js
+++ project1.js
@@ -16,16 +16,23 @@ $(function() {
   $(".square").on("click", function() {
     uniqueId = $(this).attr("id")
    console.log(uniqueId);
-   $(uniqueId)()
     $(".modal-background").show();
-    $(".modal-background").css("display", "flex")
+    $(".modal-background").css("display", "flex");
+    for(let i=0; i<qArray.length; i++){
+      console.log("hi")
+      if(qArray[i].id === uniqueId){
+        console.log("hi")
+        /*push qArray[i] to modal*/
+        $(".question").html(qArray[i].question);
+      }
+    };
+    
   });
 });
 
-[
-  uniqueId =
+qArray= [
   {
-  id:0100,
+  id:"0100",
   question: "What liquid is the most hydrating?",
   answer1: "soda",
   answer2: "water",
@@ -33,5 +40,7 @@ $(function() {
 }
 ];
 
+
+ 
 
 
