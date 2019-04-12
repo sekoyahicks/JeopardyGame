84254df8a2418fb680ebb4f2eecffd05b28203d3
Sekoya Hicks <sekoyahicks@Sekoyas-MacBook-Pro.local>
2019-04-12 11:10:25 -0400

    Got modal to work.

--- project1.js
+++ project1.js
@@ -1,9 +1,37 @@
-$(document).ready(function () {
 
-document.getElementById('button').addEventListener('click', function(){
-            
-  document.querySelector('.modal-background').style.display = 'flex';
-  }); 
-  document.querySelector('.close').addEventListener('click', function(){
-  document.querySelector('.modal-background').style.display = "none";
+/*
+  let score = 0
+
+  score = score + 1;
+
+  addEventListener().onclick
+  */
+
+//   
+
+
+let uniqueId;
+
+$(function() {
+  $(".square").on("click", function() {
+    uniqueId = $(this).attr("id")
+   console.log(uniqueId);
+   $(uniqueId)()
+    $(".modal-background").show();
+    $(".modal-background").css("display", "flex")
   });
+});
+
+[
+  uniqueId =
+  {
+  id:0100,
+  question: "What liquid is the most hydrating?",
+  answer1: "soda",
+  answer2: "water",
+  answer3: "milk"
+}
+];
+
+
+
